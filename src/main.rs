d15a4a41976f07b06c83416afa16aa6a0d378632
Félix Fischer <felix91gr@gmail.com>
2018-10-19 15:58:49 -0300

    Parser parses input from files!

--- src/main.rs
+++ src/main.rs
@@ -269,6 +269,8 @@ use clap::App;
 
 fn main() {
 
+	use std::fs;
+
 	let yaml = load_yaml!("cli.yml");
   let matches = App::from_yaml(yaml).get_matches();	
 
@@ -284,8 +286,17 @@ fn main() {
 	if matches.is_present("parse-only") {
 		println!("Running in Parser mode");
 
-    if let Some(o) = matches.value_of("INPUT") {
-      println!("Reading from input file: {}", o);
+    if let Some(input_file) = matches.value_of("INPUT") {
+      println!("Reading from input file: {}", input_file);
+
+      let unparsed_file = fs::read_to_string(input_file).expect("cannot read file");
+
+      let parse_result = parse_tc_file(&unparsed_file);
+
+      match parse_result {
+      	Ok(ast) => println!("AST: {:?}", ast),
+      	_ => println!("Could not parse file"),
+      }
 		}
 	}
 }
