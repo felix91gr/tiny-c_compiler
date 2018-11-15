63a9c9f4c23477ed2209530125c83d43d7071386
Félix Fischer <felix91gr@gmail.com>
2018-11-15 11:49:22 -0300

    Polished CLI behavior

--- src/main.rs
+++ src/main.rs
@@ -25,45 +25,48 @@ fn main() {
 
   match matches.occurrences_of("verbose") {
     0 => println!("Verbose mode is off"),
-    1 => println!("Verbose mode is kind of on"),
-    2 => println!("Verbose mode is on"),
-    3 | _ => println!("Don't be crazy"),
+    1 => println!("Verbose mode is kind on. However, it's not implemented yet!"),
+    _ => println!("With infinite verbosity."),
 	}
 
-	if matches.is_present("parse-only") {
-		println!("Running in Parser mode");
+  if let Some(mode) = matches.value_of("mode") {
+    match mode {
+      "parse" => {
+        println!("Running in Parser mode");
 
-    if let Some(input_file) = matches.value_of("INPUT") {
-      println!("Reading from input file: {}", input_file);
+        if let Some(input_file) = matches.value_of("INPUT") {
+          println!("Reading from input file: {}", input_file);
 
-      let unparsed_file = fs::read_to_string(input_file).expect("cannot read file");
+          let unparsed_file = fs::read_to_string(input_file).expect("cannot read file");
 
-      let parse_result = parse_tc_file(&unparsed_file);
+          let parse_result = parse_tc_file(&unparsed_file);
 
-      match parse_result {
-      	Ok(ast) => println!("Bare (purely syntactic) AST: \n{:#?}", ast),
-      	Err(e) => print_error(e),
-      }
-		}
-	}
-  else if matches.is_present("add-symbols") {
-    println!("Running Parser mode + Symbol propagation");
+          match parse_result {
+            Ok(ast) => println!("Naked (syntactic) AST: \n{:#?}", ast),
+            Err(e) => print_error(e),
+          }
+        }
+      },
+      "add-symbols" => {
+        println!("Running Parser mode + Symbol propagation");
 
-    if let Some(input_file) = matches.value_of("INPUT") {
-      println!("Reading from input file: {}", input_file);
+        if let Some(input_file) = matches.value_of("INPUT") {
+          println!("Reading from input file: {}", input_file);
 
-      let unparsed_file = fs::read_to_string(input_file).expect("cannot read file");
+          let unparsed_file = fs::read_to_string(input_file).expect("cannot read file");
 
-      let parse_result = parse_tc_file(&unparsed_file);
+          let parse_result = parse_tc_file(&unparsed_file);
 
-      match parse_result {
-        Ok(mut ast) => {
-          ast.enrich_interior_scope_with_symbols();
-          // println!("AST: \n{:#?}", ast)
-          println!("Enriched AST: \n{:#?}", ast)
-        },
-        Err(e) => print_error(e),
-      }
+          match parse_result {
+            Ok(mut ast) => {
+              ast.enrich_interior_scope_with_symbols();
+              println!("Enriched AST: \n{:#?}", ast)
+            },
+            Err(e) => print_error(e),
+          }
+        }
+      },
+      _ => unreachable!(),
     }
   }
 }
